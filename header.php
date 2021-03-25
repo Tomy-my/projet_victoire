fa14513e36db657f214c6c35eaf8b2d13d71cd7b
Tomy-my <azerouto@gmail.com>
2021-03-25 08:39:15 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/fonts.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="icon" href="images/favicon.png" />
+        <title># : <?php echo $title; ?></title>
+    </head>
+        <body>
+        <a href="index.php" id="titre">Titre du site</div>
+        <div class="navbar">
+            <a href="index.php" id="home"> Acceuil </a>
+            <a href="elevage.php" id="elevage"> L'élevage </a>
+            <a href="ranch.php" id="ranch"> Le ranch </a>
+            <a href="service.php" id="service"> Nos services </a>
+            <a href="contact.php" id="contact"> Contact </a>
+        </div>
